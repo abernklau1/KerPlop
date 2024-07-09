2cfb5af82e732d9a715d9a607ea83c1f8562c957
Andrew Bernklau <andrewbernklau@hotmail.com>
2024-07-09 11:04:04 -0600

    Created LevelSetup class and added methods to remove errors, Authors: Andrew, Peyton

---
+++ src/levelPieces/LevelSetup.java
@@ -0,0 +1,34 @@
+package levelPieces;
+
+import java.util.ArrayList;
+
+import gameEngine.Drawable;
+import gameEngine.Moveable;
+
+public class LevelSetup {
+
+	public void createLevel(int levelNum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Drawable[] getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Moveable> getMovingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<GamePiece> getInteractingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPlayerStartLoc() {
+		return 0;
+	}
+
+}
